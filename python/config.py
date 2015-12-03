9267681321436247ac6c905a6eb6c9ce323c5c84
Guillaume Blanc <guillaume1.blanc@orange.com>
2015-12-03 15:05:14 +0100

    delete old directories

--- python/config.py
+++
@@ -1,8 +0,0 @@
-################ FIWARE VARIABLES ################
-FIWARE_SERVER = "hackathon.villatolosa.com"
-FIWARE_PORT = 1883
-FIWARE_APIKEY = "xxxxxx"
-FIWARE_DEVICE = "myEdison"
-FIWARE_TIMEOUT = 60
-
-
